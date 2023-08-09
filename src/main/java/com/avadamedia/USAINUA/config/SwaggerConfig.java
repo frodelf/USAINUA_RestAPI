696665a12ea678b18f3438e3f2de91b6627cca2f
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-08-09 19:54:11 +0300

    bug fix

--- src/main/java/com/avadamedia/USAINUA/config/SwaggerConfig.java
+++ src/main/java/com/avadamedia/USAINUA/config/SwaggerConfig.java
@@ -4,7 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
@@ -15,4 +20,10 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://localhost:8888/USAINUA_RestAPI/");
+        return new OpenAPI().servers(List.of(server));
+    }
 }
